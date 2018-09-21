476f1539c21927c8e195a61ee684a8c732bb98f1
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-09-21 00:47:01 -0500

    updated counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,160 @@
-//This section holds variables
+//Questions, this contains all my questions
+var TriviaQuestions = [
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  },
+  {
+    q: "What year did France give the US the statue of Libery",
+    Rps: ["1908", "1808", "1886", "1888"],
+    Ans: "1886"
+  }
+];
+
+// //This section holds variables
 var Document;
-var TotalScore = 0;
-var win = 0;
-var loss = 0;
-var finalTotal;
-var randNum;
-var RandNum2;
-var buttonNumber;
-var status;
-
-//wins and losses
-$("#win").text(win);
-$("#loss").text(loss);
-//$("#TotalScore").text(TotalScore)
-
-
+var TotalWin = 0;
+var CorrectAns = 0;
+var WrongAns = 0;
+//var randQuestion;
 
 //iniate Game
-reset();
-//adds the wins
-function Winner() {
-  alert("You Beat the 4 Gummy Horseman!");
-  win++;
-  $("#win").text(win);
-  TotalScore = 0;
-  reset();
+$("#initiate-game").on("click", countdown);
+
+//Timer Code
+function countdown() {
+  //This starts each round giving you 25 seconds
+  console.log("game initiated");
+  var counter = 25;
+  document.getElementById("timer").innerHTML = counter;
+  //this is your count down
+  var timer = setInterval(function() {
+    counter -= 1;
+    document.getElementById("timer").innerHTML = counter;
+    //this is your rule
+    if (counter === 0) {
+      clearInterval(countdown);
+      countdown();
+    }
+    else {
+    var randQuestion = Math.floor(Math.random() * TriviaQuestions.length);
+    document.getElementById("randQuestion").innerHTML = question;
+    console.log(randQuestion);
+    }
+  }, 1000);
+
+  //Game Code
+  // function game() {
+  //   // this chooses a random item
+  //   var randQuestion = Math.floor(Math.random() * TriviaQuestions.length);
+  //   document.getElementById("randQuestion").innerHTML = question;
+  //   console.log(randQuestion);
+  // }
 }
-//addes the losses
-function Loser() {
-  alert("You have Diabetes!");
-  loss++;
-  $("#loss").text(loss);
-  TotalScore = 0;
-  reset();
-}
-//game reset
-function reset() {
-  RandNum = Math.floor(Math.random() * 102 + 19);
-  $("#randNum").text(RandNum);
-  console.log(RandNum);
-  buttonNumber = Math.floor(Math.random() * 12 + 1);
-  console.log(buttonNumber);
-  $("#btnClose1").val(buttonNumber);
+//console.log(randomIndex);
 
-  buttonNumber = Math.floor(Math.random() * 12 + 1);
-  console.log(buttonNumber);
-  $("#btnClose2").val(buttonNumber);
+//Need to make a reset() fucntion
+//reset()
 
-  buttonNumber = Math.floor(Math.random() * 12 + 1);
-  console.log(buttonNumber);
-  $("#btnClose3").val(buttonNumber);
+//css display attr (display none or hidden),
+// //adds the wins
+// function Winner() {
+//   alert("You Beat the 4 Gummy Horseman!");
+//   win++;
+//   $("#win").text(win);
+//   TotalScore = 0;
+//   reset();
+// }
+// //addes the losses
+// function Loser() {
+//   alert("You have Diabetes!");
+//   loss++;
+//   $("#loss").text(loss);
+//   TotalScore = 0;
+//   reset();
+// }
 
-  buttonNumber = Math.floor(Math.random() * 12 + 1);
-  console.log(buttonNumber);
-  $("#btnClose4").val(buttonNumber);
-}
-//This through my code off...thought I had to reset both functions but no...Keeping for learning purposes
-$(".button button").on("click", function() {
-  TotalScore += parseInt($(this).val());
-  console.log(TotalScore);
-  $("#TotalScore").text(TotalScore);
-  if (TotalScore === RandNum) {
-    $("#status").html(" You are a salty sugar killer - Nice Win!");
-    Winner();
-  } else if (TotalScore > RandNum) {
-    $("#status").html(" You need an insulin shot! - You lost ");
-    Loser();
-  }
-});
+// //wins and losses
+// $("#win").text(win);
+// $("#loss").text(loss);
+// //$("#TotalScore").text(TotalScore)
 
-//}
+// //game reset
+// function reset() {
+//   RandNum = Math.floor(Math.random() * 102 + 19);
+//   $("#randNum").text(RandNum);
+//   console.log(RandNum);
+//   buttonNumber = Math.floor(Math.random() * 12 + 1);
+//   console.log(buttonNumber);
+//   $("#btnClose1").val(buttonNumber);
 
-//});
+//   buttonNumber = Math.floor(Math.random() * 12 + 1);
+//   console.log(buttonNumber);
+//   $("#btnClose2").val(buttonNumber);
+
+//   buttonNumber = Math.floor(Math.random() * 12 + 1);
+//   console.log(buttonNumber);
+//   $("#btnClose3").val(buttonNumber);
+
+//   buttonNumber = Math.floor(Math.random() * 12 + 1);
+//   console.log(buttonNumber);
+//   $("#btnClose4").val(buttonNumber);
+// }
+// //This through my code off...thought I had to reset both functions but no...Keeping for learning purposes
+// $(".button button").on("click", function() {
+//   TotalScore += parseInt($(this).val());
+//   console.log(TotalScore);
+//   $("#TotalScore").text(TotalScore);
+//   if (TotalScore === RandNum) {
+//     $("#status").html(" You are a salty sugar killer - Nice Win!");
+//     Winner();
+//   } else if (TotalScore > RandNum) {
+//     $("#status").html(" You need an insulin shot! - You lost ");
+//     Loser();
+//   }
+// });
+
+// //}
+
+// //});
