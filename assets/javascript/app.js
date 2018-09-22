57928dcf14809207d99d4f38ef2ddf7155b4d97c
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2018-09-21 19:53:49 -0500

    upate game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,21 @@
 //Questions, this contains all my questions
 var TriviaQuestions = [
   {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
+    question: "What year did France gift the United States with the statue of Libery?",
+    response: ["1908", "1808", "1886", "1888"],
+    answer: "1886"
   },
   {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
+    question: "Which of these bridges connects Manhattan to Brooklyn?",
+    response: ["Manhattan", "Brooklyn", "Verrazano", "Goethals"],
+    answer: "Brooklyn"
   },
   {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
+    question: "Which building did Frank Lloyd Wright Design?",
+    response: ["30 Rockefeller Place", "Woolworth Building", "Flatiron Building", "Solomon R. Guggenheim Museum"],
+    answer: "Solomon R. Guggenheim Museum"
   },
-  {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
-  },
-  {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
-  },
-  {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
-  },
-  {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
-  },
-  {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
-  },
-  {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
-  },
-  {
-    q: "What year did France give the US the statue of Libery",
-    Rps: ["1908", "1808", "1886", "1888"],
-    Ans: "1886"
-  }
+  
 ];
 
 // //This section holds variables
@@ -57,41 +23,54 @@ var Document;
 var TotalWin = 0;
 var CorrectAns = 0;
 var WrongAns = 0;
+var randomIndex = " ";
 //var randQuestion;
 
 //iniate Game
-$("#initiate-game").on("click", countdown);
+$("#initiate-game").on("click", game);
+//console.log(game)
 
-//Timer Code
-function countdown() {
-  //This starts each round giving you 25 seconds
-  console.log("game initiated");
-  var counter = 25;
-  document.getElementById("timer").innerHTML = counter;
-  //this is your count down
-  var timer = setInterval(function() {
-    counter -= 1;
-    document.getElementById("timer").innerHTML = counter;
-    //this is your rule
-    if (counter === 0) {
-      clearInterval(countdown);
-      countdown();
-    }
-    else {
-    var randQuestion = Math.floor(Math.random() * TriviaQuestions.length);
-    document.getElementById("randQuestion").innerHTML = question;
-    console.log(randQuestion);
-    }
-  }, 1000);
-
-  //Game Code
-  // function game() {
-  //   // this chooses a random item
-  //   var randQuestion = Math.floor(Math.random() * TriviaQuestions.length);
-  //   document.getElementById("randQuestion").innerHTML = question;
-  //   console.log(randQuestion);
-  // }
+ //Game Code
+   function game() {
+    //In order to change the index to an array we need to convert it
+    var randomIndex = getRandomInt(0, (question.length-1))
+    document.getElementById("randQuestion").innerHTML = randomIndex;
+    
+    //this should display question
+      console.log(answerArray)
+    // $.each(randQuestion, function(key, value){
+    //   $("#randQuestion").append(Key + ": " + value + '<br>');
+    // }
+    // )}
 }
+
+
+
+
+
+// //Timer Code
+// function countdown() {
+//   //This starts each round giving you 25 seconds
+//   console.log("game initiated");
+//   var counter = 25;
+//   document.getElementById("timer").innerHTML = counter;
+//   //this is your count down
+//   var timer = setInterval(function() {
+//     counter -= 1;
+//     document.getElementById("timer").innerHTML = counter;
+//     //this is your rule
+//     if (counter === 0) {
+//       clearInterval(countdown);
+//       countdown();
+//     }
+//     else {
+//     var randQuestion = Math.floor(Math.random() * TriviaQuestions.length);
+//     document.getElementById("randQuestion").innerHTML = question;
+//     console.log(randQuestion);
+//     }
+//   }, 1000);
+
+ 
 //console.log(randomIndex);
 
 //Need to make a reset() fucntion
